f9ff2ef56e2d1ffc7220a130baf9cccf9ff577ff
enriquejh <enriquejjoaquinh@gmail.com>
2024-12-17 21:30:48 -0600

    Solve exercise 5
    The function created sums all the integers between the two parameters
    passed. It verifies that the first parameter is smaller than the second
    parameter, otherwise it swaps them. It also verifies that both
    parameters are integers, otherwise it returns error.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(sumStart, sumEnd) {
+    let sum = 0;
 
+    const largerNumFirst = sumStart > sumEnd;
+    const negativeParams = sumStart < 0 || sumEnd < 0;
+    const nonIntegerParams = !Number.isInteger(sumStart) || !Number.isInteger(sumEnd);
+
+    if (largerNumFirst){
+        let temp = sumStart;
+        sumStart = sumEnd;
+        sumEnd = temp;
+    }
+    if (negativeParams || nonIntegerParams) {
+        return 'ERROR';
+    }
+
+    for (let i = sumStart; i <= sumEnd; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
