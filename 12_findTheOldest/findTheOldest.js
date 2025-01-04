4cb641879b600ec7a8bf1273d0c67a31375e3c2c
enriquejh <enriquejjoaquinh@gmail.com>
2025-01-03 22:47:07 -0600

    Solve exercise 12
    The function creates a new array with object elements that have as
    properties the names and the ages of each pearson. Then, it sorts the
    array based on age from oldest to youngest, and returns the object from
    the original array that has the same name as the first element of the
    new array. If the original element doesn't have a year of death, it uses
    the current year to calculate the age of the pearson

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let agePeople = people.map(pearson => {
+        let lastYear = 0;
+        if (pearson.yearOfDeath === undefined){
+            lastYear = new Date().getFullYear();
+        } else {
+            lastYear = pearson.yearOfDeath;
+        }
 
+        return { name: pearson.name, age: lastYear - pearson.yearOfBirth }
+    });
+    agePeople.sort((a, b) => b.age - a.age);
+
+    return people.find(pearson => pearson.name === agePeople[0].name);
 };
 
 // Do not edit below this line
