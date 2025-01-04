6bcf25af91d0920221cd6ccf5161ccf569f8707d
enriquejh <enriquejjoaquinh@gmail.com>
2025-01-03 20:25:11 -0600

    Solve exercise 8
    One function was created for subtraction, multiplication, power, and
    factorial. For power, a for loop was used, meanwhile for factorial the
    function was recursive. Two functions were created for addition, the
    first admits two arguments, the second admits just one but it has to be
    an array. For the latter, the reduce function for arrays was used

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(operands) {
+	return operands.reduce((total, operand) => total + operand, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(factors) {
+  return factors.reduce((total, factor) => total * factor, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = 1;
+  for (let i = 0; i < b; i++){
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a <= 1){
+    return 1;
+  }
+  else {
+    return a * factorial(a - 1);
+  }
 };
 
 // Do not edit below this line
