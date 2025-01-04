dbe4e1ef2a04a495652640f64f38232741bd9c8d
enriquejh <enriquejjoaquinh@gmail.com>
2025-01-03 21:52:12 -0600

    Solve exercise 10
    The function created is a recursive function. Its special cases are when
    the input is a negative number (just returns an string to
    indicate that there was an error), 0 (returns 0), and 1 and 2 (returns
    1), in any other case returns the sum of the last two positions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    let i = +position;
+    if (i < 0)
+        return 'OOPS';
+    if (i == 0)
+        return 0;
+    else if (i == 1 || i == 2)
+        return 1;
+    return fibonacci(i - 1) + fibonacci(i - 2)
 };
 
 // Do not edit below this line
