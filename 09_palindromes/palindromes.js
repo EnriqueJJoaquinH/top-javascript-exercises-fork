9cb1d2776c60fc0b4b62040a74a44abb8c5be885
enriquejh <enriquejjoaquinh@gmail.com>
2025-01-03 21:36:49 -0600

    Solve exercise 9
    The function created receives a string, and checks if it is a
    palindrome. The split function for strings and the filter, map, reverse,
    every, and spread operator for arrays are used. Plus, a regular
    expression was used to filter alphanumeric characters

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let stringArr = string.split('')
+                          .filter((char) => /[a-z0-9]/i.test(char))
+                          .map((char) => char.toLowerCase());
+    let reverseArr = [...stringArr].reverse();
+    return stringArr.every((element, index) => element === reverseArr[index]);
 };
 
 // Do not edit below this line
